0406239c0098ce3dd2441081ffeda24101e7332a
Ben Lebherz <git@benleb.de>
2019-11-11 11:30:25 +0100

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='adutils',
-      version='0.4.4',
+      version='0.4.5',
       license='MIT',
       url='http://github.com/benleb/adutils',
       author='Ben Lebherz',
