4d74802483f0e66d7e93464c4c35d4c2868bc2c4
Ben Lebherz <git@benleb.de>
2020-12-31 10:58:25 +0100

    bump to 0.9.0

--- apps/automoli/automoli.py
+++ apps/automoli/automoli.py
@@ -17,7 +17,7 @@ from typing import Any, Coroutine, Dict, Iterable, List, Optional, Set, Union
 import hassapi as hass
 
 
-__version__ = "0.9.0b1"
+__version__ = "0.9.0"
 
 APP_NAME = "AutoMoLi"
 APP_ICON = "💡"
